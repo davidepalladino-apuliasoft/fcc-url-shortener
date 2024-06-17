f1b744502b616dad357ba9128a1ef7474ed6403b
Davide Palladino <davide.palladino@apuliasoft.com>
2024-06-17 18:46:30 +0200

    Finalized project

--- index.js
+++ index.js
@@ -2,16 +2,43 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
+
+const urls = {}
+let counter = 0;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use('/', bodyParser.urlencoded({extended: false}));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const url = new String(req.body.url);
+ 
+  if (url.match('^http[s]?:\/\/[a-z0-9.\-=?\/]+')) {
+    urls[counter] = url;
+
+    res.json({ 
+      original_url: req.body.url,
+      short_url: counter
+    });
+  } else {
+    console.log('error')
+  }
+});
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const shortUrl = req.params.short_url
+  res.redirect(urls[shortUrl])
 });
 
 // Your first API endpoint
